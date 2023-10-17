45a5da8387f01e7081b0b3f4c97e939b91efd342
Nguyen Huynh <nhuynh9497@gmail.com>
2023-10-17 11:55:41 -0400

    fetched images and randomize them. Upon clicking an image, all images will be randomized again.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {useEffect, useState} from "react";
 import {fetchImages} from "./data/dataFetcher.js";
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
 
@@ -11,29 +12,58 @@ function App() {
     // set to false means that we don't fetch; otherwise, fetch.
     const [fetchImgs, setFetchImgs] = useState(true);
 
+    function cardClickHandler(){
+        randomizeImages(imgs);
+    }
+    function randomizeImages(imgs) {
+        const curImgs = [...imgs];
+        const randomImgs = [];
+        const s = new Set();
+        for (let i = 0; i < curImgs.length; i++) {
+            let randIdx = Math.floor(Math.random() * curImgs.length);
+            while (s.has(randIdx)) {
+                randIdx = Math.floor(Math.random() * curImgs.length);
+            }
+            s.add(randIdx);
+            randomImgs.push(curImgs[randIdx]);
+        }
+        setImgs(randomImgs);
+    }
+
+    function initializeImages(imgRes) {
+        const cards = (imgRes.map(img => {
+            return {id: uuidv4(), link: img}
+        }))
+        randomizeImages(cards);
+    }
+    console.log('yo');
     // first we use useEffect to fetch data from external API.
     // then display list of images.
     // we might need to share image list state.
-    console.log('yo');
     useEffect(() => {
-        if (fetchImgs){
+        if (fetchImgs) {
             fetchImages("https://api.pexels.com/v1/search", "animals")
-                .then(imgRes => setImgs(imgRes))
+                .then(imgRes => {
+                    initializeImages(imgRes)
+                })
             setFetchImgs(false);
         }
 
-        return ()=>{
+        return () => {
             setImgs([]);
             setFetchImgs(false);
         }
 
     }, [fetchImgs]);
+
     return (
         <ul>
-            {imgs.map(img=>{
+            {imgs.map(img => {
                 return (
-                    <li>
-                        <img src={img}></img>
+                    <li key={img.id}
+                    onClick={() => cardClickHandler() }
+                    >
+                        <img src={img.link} alt="img"></img>
                     </li>
                 )
             })}
